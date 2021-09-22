51edd5354887be64fc0baed7962de08dde8ba7ea
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-22 17:53:57 -0300

    Criado as funções thecocktaildbFetch e themealdbFetch

--- src/components/SearchBarHeader.js
+++ src/components/SearchBarHeader.js
@@ -3,19 +3,50 @@ import { useHistory } from 'react-router-dom';
 
 const optionsDefault = {
   checkRadio: '',
-  input: 'valor',
+  input: 'g',
   pageName: '',
+};
+
+// Fetch para as comidas
+// Vefirica qual radio foi selecionado e criar endpoint correto usando o input digitado.
+const themealdbFetch = async (checkRadio, input) => {
+  const themealdbEndPoint = {
+    ingredient_search: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`,
+    name_search: `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`,
+    first_letter_search: `https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`,
+  };
+
+  const themealdb = (await (await fetch(themealdbEndPoint[checkRadio])).json());
+  console.table(themealdb);
+  return themealdbEndPoint;
+};
+
+// Fetch para as Bebidas
+// Vefirica qual radio foi selecionado e criar endpoint correto usando o input digitado.
+const thecocktaildbFetch = async (checkRadio, input) => {
+  const themealdbEndPoint = {
+    ingredient_search: `https:/www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input}`,
+    name_search: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`,
+    first_letter_search: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${input}`,
+  };
+
+  const thecocktaildb = (await (await fetch(themealdbEndPoint[checkRadio])).json());
+  console.table(thecocktaildb);
+  return themealdbEndPoint;
 };
 
 export default function SearchBarHeader() {
   // Pega a Url e assim sei qual e a pagina que estou.
   const pageName = useHistory().location.pathname;
 
-  const [options, setoptions] = useState({ ...optionsDefault, pageName });
+  const [options, setOptions] = useState({ ...optionsDefault, pageName });
 
   const handleClick = () => {
     const { checkRadio, input } = options;
-    console.log(checkRadio, input, pageName);
+
+    // logica para escolhe a fetch correta
+    themealdbFetch(checkRadio, input);
+    thecocktaildbFetch(checkRadio, input);
   };
 
   return (
@@ -26,8 +57,8 @@ export default function SearchBarHeader() {
           id="ingredient-search-radio"
           name="radio"
           data-testid="ingredient-search-radio"
-          value="ingredient-search-radio"
-          onChange={ (e) => setoptions({ ...options, checkRadio: e.target.value }) }
+          value="ingredient_search"
+          onChange={ (e) => setOptions({ ...options, checkRadio: e.target.value }) }
         />
         Ingrediente
       </label>
@@ -37,8 +68,8 @@ export default function SearchBarHeader() {
           id="name-search-radio"
           name="radio"
           data-testid="name-search-radio"
-          value="name-search-radio"
-          onChange={ (e) => setoptions({ ...options, checkRadio: e.target.value }) }
+          value="name_search"
+          onChange={ (e) => setOptions({ ...options, checkRadio: e.target.value }) }
 
         />
         Nome
@@ -49,8 +80,8 @@ export default function SearchBarHeader() {
           id="first-letter-search-radio"
           name="radio"
           data-testid="first-letter-search-radio"
-          value="first-letter-search-radio"
-          onChange={ (e) => setoptions({ ...options, checkRadio: e.target.value }) }
+          value="first_letter_search"
+          onChange={ (e) => setOptions({ ...options, checkRadio: e.target.value }) }
         />
         Primeira letra
       </label>
