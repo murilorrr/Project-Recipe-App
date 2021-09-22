cb2e62980d5ea9ef79b7ce28e5c14c9f5eccc90b
suco <francalucas937@gmail.com>
2021-09-22 17:37:07 -0300

    pull main-group-28-dev

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Provider } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import appProvider from './contextAPI/appProvider';
+// import appProvider from './contextAPI/appProvider';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RecipeFoods from './pages/RecipeFoods';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={ appProvider }>
-        <Switch>
-          <Route path="/comidas" component={ comidas } />
-          <Route path="/bebidas" component={ bebidas } />
+      {/* <Provider store={ appProvider }> */}
+      <Switch>
+        <Route path="/comidas" component={ RecipeFoods } />
+        {/*  <Route path="/bebidas" component={ bebidas } />
           <Route path="/comidas/:id" component={ comidasDetails } />
-          <Route path="/bebidas/:id" component={ bebidasDetails } />
-          <Route exact path="/" component={ Login } />
-        </Switch>
-      </Provider>
+          <Route path="/bebidas/:id" component={ bebidasDetails } /> */}
+        <Route exact path="/" component={ Login } />
+      </Switch>
+      {/* </Provider> */}
     </BrowserRouter>
   );
 }
