778b2b307527c9ed5cb32c40a4a67bfb169067a0
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-23 13:56:39 -0300

    Preparando pra merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import appProvider from './contextAPI/appProvider';
+import appProvider from './contextAPI/appProvider';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBarHeader from './components/SearchBarHeader';
@@ -8,17 +8,16 @@ import SearchBarHeader from './components/SearchBarHeader';
 function App() {
   return (
     <BrowserRouter>
-      {/* <Provider store={ appProvider }> */}
-      <Switch>
-        <Route path="/comidas" component={ SearchBarHeader } />
-        <Route path="/bebidas" component={ SearchBarHeader } />
-        {/*
+      <Provider store={ appProvider }>
+        <Switch>
+          <Route path="/comidas" component={ SearchBarHeader } />
+          <Route path="/bebidas" component={ SearchBarHeader } />
           <Route path="/comidas/:id" component={ comidasDetails } />
           <Route path="/bebidas/:id" component={ bebidasDetails } />
-          <Route exact path="/" component={ Login } /> */}
-        <Route exact path="/teste" component={ SearchBarHeader } />
-      </Switch>
-      {/* </Provider> */}
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/teste" component={ SearchBarHeader } />
+        </Switch>
+      </Provider>
     </BrowserRouter>
   );
 }
--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,25 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MiniCard({ args }) {
   const { i: index = '' } = args;
   return (
-    <section data-testid={ `${index}-recipe-card` }>
-      <section>
-        <img
-          Style="width:250px"
-          src={ args.strMealThumb || args.strDrinkThumb || '' }
-          alt={ args.strMeal }
-          data-testid={ `${args.index}-card-img` }
-        />
-        <h3
-          data-testid={ `${args.index}-card-name` }
-        >
-          {args.strMeal || args.strDrink || ''}
+    <Link to={ `/dfasdfasdfa/${args.idMeal || args.idDrink}` }>
+      <section data-testid={ `${index}-recipe-card` }>
+        <section>
+          <img
+            Style="width:250px"
+            src={ args.strMealThumb || args.strDrinkThumb }
+            alt={ args.strMeal }
+            data-testid={ `${args.index}-card-img` }
+          />
+          <h3
+            data-testid={ `${args.index}-card-name` }
+          >
+            {args.strMeal || args.strDrink }
 
-        </h3>
+          </h3>
+        </section>
       </section>
-    </section>
+    </Link>
   );
 }
 
