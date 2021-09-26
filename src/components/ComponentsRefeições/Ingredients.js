c9c0d8b44db07aacefb94c5b6ec3a9c06f4e9cd8
Murilo Ribeiro <murilorsv14@gmail.com>
2021-09-26 09:17:25 -0300

    proptypes and fix lint

--- src/components/ComponentsRefeições/Ingredients.js
+++ src/components/ComponentsRefeições/Ingredients.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 
-function ingredients({item}) {
+function ingredients({ item }) {
   const getValuesInObject = (obj, value) => {
-        const lista = [];
-        Object.keys(obj).forEach((key) => {
-            if (key.includes(value) && obj[key] !== '' && obj[key] !== null) {
-            lista.push(obj[key]);
-            }
-        });
-        return lista;
-        };
-    
-  const ingredients = getValuesInObject(item[0], 'strIngredient');
+    const lista = [];
+    Object.keys(obj).forEach((key) => {
+      if (key.includes(value) && obj[key] !== '' && obj[key] !== null) {
+        lista.push(obj[key]);
+      }
+    });
+    return lista;
+  };
+
+  const ingredientsList = getValuesInObject(item[0], 'strIngredient');
   const ingredientsMeansure = getValuesInObject(item[0], 'strMeasure');
-    return(
-        <div className="ingredients">
-        <ul>
-          {ingredients.map((ingredient, index) => (
-            <li key={ ingredient } data-testid={ `${index}-ingredient-name-and-measure` }>
-              <span>{ingredient}</span>
-              <span>{ingredientsMeansure[index]}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  return (
+    <div className="ingredients">
+      <ul>
+        {ingredientsList.map((ingredient, index) => (
+          <li key={ ingredient } data-testid={ `${index}-ingredient-name-and-measure` }>
+            <span>{ingredient}</span>
+            <span>{ingredientsMeansure[index]}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ingredients
+export default ingredients;
--- src/components/ComponentsRefeições/Instruction.js
+++ src/components/ComponentsRefeições/Instruction.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Instruction({strInstructions}) {
-    return(
-        <p data-testid="instructions">
-          {strInstructions}
-        </p>
-    );
+function Instruction({ strInstructions }) {
+  return (
+    <p data-testid="instructions">
+      {strInstructions}
+    </p>
+  );
 }
+
+Instruction.propTypes = {
+  strInstructions: PropTypes.string.isRequired,
+};
 
 export default Instruction;
