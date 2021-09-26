b3b4be3a5eb227553131eef3be878ddf59821aaf
Murilo Ribeiro <murilorsv14@gmail.com>
2021-09-26 08:58:46 -0300

    general componentizer details

--- src/pages/DrinkDetails.js
+++ src/pages/DrinkDetails.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { FavoriteButton, CarrouselRecomendations, ShareButton,
   Loading, StartRecipe } from '../components';
 import HeaderRecipes from '../components/ComponentsRefeições/HeaderRecipes';
+import Ingredients from '../components/ComponentsRefeições/Ingredients';
+import Instruction from '../components/ComponentsRefeições/Instruction';
 
 function DrinkDetails(props) {
   const { match: { params: { id } }, location, history } = props;
@@ -29,21 +31,8 @@ function DrinkDetails(props) {
     fetchAndSet();
   }, [id]);
 
-  const getValuesInObject = (obj, value) => {
-    const lista = [];
-    Object.keys(obj).forEach((key) => {
-      if (key.includes(value) && obj[key] !== '' && obj[key] !== null) {
-        lista.push(obj[key]);
-      }
-    });
-    return lista;
-  };
-
   if (item.length === 0) return (<Loading />);
   const { strAlcoholic, strDrinkThumb, strDrink, strInstructions } = item[0];
-
-  const ingredients = getValuesInObject(item[0], 'strIngredient');
-  const ingredientsMeansure = getValuesInObject(item[0], 'strMeasure');
 
   return (
     <div>
@@ -60,23 +49,9 @@ function DrinkDetails(props) {
           item={ item }
         />
       </div>
-      <div className="ingredients">
-        <ul>
-          {ingredients.map((ingredient, index) => (
-            <li
-              key={ ingredient }
-              data-testid={ `${index}-ingredient-name-and-measure` }
-            >
-              <span>{ingredient}</span>
-              <span>{ingredientsMeansure[index]}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Ingredients item={ item } />
       <div className="instructions">
-        <p data-testid="instructions">
-          {strInstructions}
-        </p>
+        <Instruction strInstructions={ strInstructions } />
       </div>
       <h3>Recomendados</h3>
       <CarrouselRecomendations recomendation={ recomendation } />
