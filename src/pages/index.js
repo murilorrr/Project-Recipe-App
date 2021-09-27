15515551e6ecfeeec2aa37c054b5e65d99aebb97
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-27 01:04:24 -0300

    Update Index pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 export { default as DrinkDetails } from './DrinkDetails';
 export { default as Drinks } from './Drinks';
-export { default as Explorar } from './Explore';
+export { default as Explore } from './Explore';
 export { default as ExploreDrink } from './ExploreDrink';
 export { default as ExploreDrinkIngred } from './ExploreDrinkIngred';
 export { default as ExploreFood } from './ExploreFood';
 export { default as ExploreFoodIngred } from './ExploreFoodIngred';
+export { default as ExploreFoodOrDrink } from './ExploreFoodOrDrink';
 export { default as FavoriteRecipes } from './FavoriteRecipes';
 export { default as FoodDetails } from './FoodDetails';
 export { default as Foods } from './Foods';
@@ -12,4 +13,6 @@ export { default as Login } from './Login';
 export { default as NotFound } from './NotFound';
 export { default as OriginFood } from './OriginFood';
 export { default as Profile } from './Profile';
+export { default as RecipeFoods } from './RecipeFoods';
 export { default as RecipesMade } from './RecipesMade';
+
