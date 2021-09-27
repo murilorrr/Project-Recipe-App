1ec8833602b05ab9b638e3a8f92af88a4f5bc3e5
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-27 02:36:21 -0300

    Mexendo no css do minicard

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../CSS/minicar.css';
 
 export default function MiniCard({ args }) {
   const { i: index = '', page } = args;
   return (
     <Link to={ `${page}/${args.idMeal || args.idDrink}` }>
-      <section>
+      <section className="mini-card-food-container">
         <section data-testid={ `${index}-recipe-card` }>
           <img
             style={ { width: '250px' } }
