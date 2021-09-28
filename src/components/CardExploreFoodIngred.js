17072dea1fa45f7e9f85378179fc5db00e5490c9
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-27 23:58:41 -0300

    Criando o html do CardExploreFoodIngred

--- src/components/CardExploreFoodIngred.js
+++ src/components/CardExploreFoodIngred.js
@@ -1,13 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-function CardExploreFoodIngred() {
+function CardExploreFoodIngred({ index, name }) {
   return (
-    <div data-testid={ `${index}-ingredient-card` }>
+    <div className="card-explore-food-ingred" data-testid={ `${index}-ingredient-card` }>
       <img src="" alt="" data-testid={ `${index}-card-img` } />
       <p data-testid={ `${index}-card-name` }>{ name }</p>
-
     </div>
   );
 }
+
+CardExploreFoodIngred.propTypes = {
+  index: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default CardExploreFoodIngred;
