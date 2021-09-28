c509df60da85594f30666e3817631194db8e032d
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-28 00:32:15 -0300

    Criando o useEffect para a requisição http

--- src/components/CardExploreFoodIngred.js
+++ src/components/CardExploreFoodIngred.js
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function CardExploreFoodIngred({ index, name }) {
+const urlImageBase = 'https://www.themealdb.com/images/ingredients/';
+
+function CardExploreFoodIngred({ index, strIngredient }) {
   return (
     <div className="card-explore-food-ingred" data-testid={ `${index}-ingredient-card` }>
-      <img src="" alt="" data-testid={ `${index}-card-img` } />
-      <p data-testid={ `${index}-card-name` }>{ name }</p>
+      <img
+        Style="width: 250px"
+        src={ `${urlImageBase}/${strIngredient}.png` }
+        alt={ `Foto de uma ingrediente chamando ${strIngredient}` }
+        data-testid={ `${index}-card-img` }
+      />
+      <p data-testid={ `${index}-card-name` }>{ strIngredient }</p>
     </div>
   );
 }
 
 CardExploreFoodIngred.propTypes = {
   index: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+  strIngredient: PropTypes.string.isRequired,
 };
 
 export default CardExploreFoodIngred;
--- src/pages/ExploreFoodIngred.js
+++ src/pages/ExploreFoodIngred.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderNoSearch from '../components/HeaderNoSearch';
 import Footer from '../components/Footer';
+import { CardExploreFoodIngred } from '../components';
 
+const URL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+const MAX_INDEX = 12;
 function ExploreFoodIngred() {
+  const [listIngred, setlistIngred] = useState();
+
+  useEffect(() => {
+    const fectIngred = async () => {
+      const request = await fetch(URL);
+      const result = await request.json();
+      setlistIngred(result.meals.slice(0, MAX_INDEX));
+      console.log('request');
+    };
+    fectIngred();
+  }, []);
+
+  if (!listIngred) return null;
+  console.log(listIngred);
   return (
     <div>
       <HeaderNoSearch word="Explorar Ingredientes" />
+      {listIngred.map((item, index) => (<CardExploreFoodIngred
+        key={ index }
+        index={ index }
+        strIngredient={ item.strIngredient }
+      />))}
       <Footer />
     </div>
   );
