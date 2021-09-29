abb8ca9899fe398aebf4757a50da93cf45c873eb
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-09-29 16:21:08 -0300

    Em andamento.

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // const MAX_LENGHT = 6;
 
 function CategoryButton() {
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const request = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      
+    }
+  })
+
   return (
-    <button type="button">
+    <button type="button" data-testid=''>
       Teste.
     </button>
   );
