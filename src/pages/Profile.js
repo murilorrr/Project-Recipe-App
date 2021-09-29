9bfac566d0b1837b83e7d78a54a43fc8b4c0ded2
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-29 00:48:41 -0300

    Todos os requistos passando

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderNoSearch from '../components/HeaderNoSearch';
 import Footer from '../components/Footer';
 
@@ -9,9 +10,38 @@ function Profile() {
       <HeaderNoSearch word="Perfil" />
       <div>
         <h3 data-testid="profile-email">{ email }</h3>
-        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-        <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-        <button type="button" data-testid="profile-logout-btn">Sair</button>
+
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+
+          </button>
+
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+
+          </button>
+
+        </Link>
+        <Link to="/">
+          <button
+            onClick={ () => localStorage.clear() }
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Sair
+
+          </button>
+
+        </Link>
       </div>
       <Footer />
     </div>
