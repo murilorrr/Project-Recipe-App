4ce8b0582b8a0ceadc3325c73e7a53c9c136eab5
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-09-29 20:20:50 -0300

    Falta fazer o if pra abarcar as bebidas.

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// const MAX_LENGHT = 6;
+// Magic number que o lint vai pedir.
+const MAX_LENGHT = 5;
 
 function CategoryButton() {
-
+  // Aqui é o estado do componente.
+  const [state, setState] = useState([]);
+  // Aqui é o equivalente ao componenteDidMount.
   useEffect(() => {
+    // Quando estamos utilizando o useEffect, precisamos fazer o async/await dentro da função e não nela.
     const fetchApi = async () => {
       const request = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
-      
-    }
-  })
+      const data = await request.json();
+      // SetState atualiza o estado do componente (state).
+      setState(data.meals.slice(0, MAX_LENGHT));
+      return data;
+    };
+    // O fetchApi é uma função que busca o conteudo da URL, faz um flitro no contéudo buscado (const data).
+    fetchApi();
+  }, []); // O parâmetro "[]" é o default para que o código não entre em loop.
 
   return (
-    <button type="button" data-testid=''>
-      Teste.
-    </button>
+    <div>
+      <button type="button">
+        All
+      </button>
+      {state.map((item, index) => (
+        <button
+          type="button"
+          key={ index }
+          data-testid={ `${item.strCategory}-category-filter` }
+        >
+          {item.strCategory}
+        </button>))}
+    </div>
   );
 }
 
