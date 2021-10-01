816af8a6d1c87093e7a7ad4ca683f0c75994a58e
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-09-30 23:42:11 -0300

    Refatorando SearchBarHeader

--- src/components/SearchBarHeader.js
+++ src/components/SearchBarHeader.js
@@ -2,90 +2,53 @@ import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Context from '../contextAPI/Context';
 
-const optionsDefault = {
-  checkRadio: '',
-  pageName: '',
-  listThecocktailOrThemeal: [],
-  loading: true,
-};
-
-const MAX_INDEX = 12;
-
 // Fetch para as comidas
 // Vefirica qual radio foi selecionado e criar endpoint correto usando o input digitado.
-const themealdbFetch = async (params) => {
-  const { checkRadio, input, options, setOptions, setListItem } = params;
+const themealdbFetch = (checkRadio, input, setBaseUrlFood) => {
   const themealdbEndPoint = {
     ingredient_search: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`,
     name_search: `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`,
     first_letter_search: `https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`,
   };
+  setBaseUrlFood(themealdbEndPoint[checkRadio]);
 
-  const themealdb = (await (await fetch(themealdbEndPoint[checkRadio])).json());
-  // se themealdb for null retorna lista [].
-  setOptions(
-    { ...options,
-      listThecocktailOrThemeal:
-      themealdb.meals ? themealdb.meals.slice(0, MAX_INDEX) : [],
-      loading: true },
-  );
-  setListItem(themealdb.meals ? themealdb.meals.slice(0, MAX_INDEX) : []);
-
-  if (!themealdb.meals) {
-    global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-  }
-  return themealdb;
+  // if (!themealdb.meals) {
+  //   global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+  // }
 };
 
 // Fetch para as Bebidas
 // Vefirica qual radio foi selecionado e criar endpoint correto usando o input digitado.
-const thecocktaildbFetch = async (params) => {
-  const { checkRadio, input, options, setOptions, setListItem } = params;
+const thecocktaildbFetch = async (checkRadio, input, setBaseUrlDrink) => {
   const themealdbEndPoint = {
     ingredient_search: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input}`,
     name_search: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`,
     first_letter_search: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${input}`,
   };
 
-  const thecocktaildb = (await (await fetch(themealdbEndPoint[checkRadio])).json());
-  if (!thecocktaildb.drinks) {
-    return global.alert(
-      'Sinto muito, não encontramos nenhuma receita para esses filtros.',
-    );
-  }
-  setOptions(
-    { ...options,
-      listThecocktailOrThemeal:
-      thecocktaildb.drinks ? thecocktaildb.drinks.slice(0, MAX_INDEX) : [],
-      loading: true },
-  );
-  setListItem(thecocktaildb.drinks ? thecocktaildb.drinks.slice(0, MAX_INDEX) : []);
-  return thecocktaildb;
+  setBaseUrlDrink(themealdbEndPoint[checkRadio]);
+
+  // return global.alert(
+  //   'Sinto muito, não encontramos nenhuma receita para esses filtros.',
+  // );
 };
 
 export default function SearchBarHeader() {
+  const [options, setOptions] = useState();
   // Pega a Url e assim sei qual e a pagina que estou.
   const pageName = useHistory().location.pathname;
-  const { setListItem } = useContext(Context);
-
-  const [options, setOptions] = useState({ ...optionsDefault, pageName });
+  const { setBaseUrlFood, setBaseUrlDrink } = useContext(Context);
 
   const handleClick = () => {
     const { checkRadio } = options;
     const input = document.getElementById('search-input').value;
-    setOptions({ ...options, loading: false });
-
-    if (input.length > 1 && checkRadio === 'first_letter_search') {
-      setOptions({ ...options, loading: true });
-      return global.alert('Sua busca deve conter somente 1 (um) caracter');
-    }
 
     if (pageName === '/comidas') {
-      return themealdbFetch({ checkRadio, input, options, setOptions, setListItem });
+      return themealdbFetch(checkRadio, input, setBaseUrlFood);
     }
 
     if (pageName === '/bebidas') {
-      return thecocktaildbFetch({ checkRadio, input, options, setOptions, setListItem });
+      return thecocktaildbFetch(checkRadio, input, setBaseUrlDrink);
     }
   };
 
@@ -106,7 +69,7 @@ export default function SearchBarHeader() {
           name="radio"
           data-testid="ingredient-search-radio"
           value="ingredient_search"
-          onChange={ (e) => setOptions({ ...options, checkRadio: e.target.value }) }
+          onChange={ (e) => setOptions({ checkRadio: e.target.value }) }
         />
         Ingrediente
       </label>
